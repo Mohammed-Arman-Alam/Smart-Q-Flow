991b75b77c349171852c453db4b34db594a8b953
MaYa <armanalam2410@gmail.com>
2026-05-07 22:41:36 +0600

    deploy

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://smart-q-flow-server.vercel.app'
 })
 const useAxios =()=>{
     return axiosInstance;
